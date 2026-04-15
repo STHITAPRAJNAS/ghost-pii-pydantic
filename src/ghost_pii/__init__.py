cdc5bcd760f22ce115322047ac056fcf2186db40
Sthitaprajna Sahoo <papu.sahoo@gmail.com>
2026-04-15 17:02:19 -0400

    fix: update __version__ in __init__.py to 0.2.1

--- src/ghost_pii/__init__.py
+++ src/ghost_pii/__init__.py
@@ -3,7 +3,7 @@ from .pydantic import PII, masked_pii, wrap_pii
 from .context import set_strict_mode, unmask_pii
 from .inspector import add_unsafe_module, remove_unsafe_module
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __all__ = [
     "GhostString",
     "MaskStrategy",
